323fa11d9703a854065e2d858fd4dd70aa26dce4
Gus Liakos <Gliakos@cnm.edu>
2018-04-23 15:49:52 -0600

    set comment to-do list

---
+++ php/classes/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gusli
+ * Date: 4/19/2018
+ * Time: 2:52 PM
+ *
+ * need: validate UUID (Ramsey)
+ * state vars
+ * constructor
+ * doc blocks for it all
+ * 	oml assignment.sql
+ * accessor &mutator methods for every state var
+ */
+class Profile {
+	/**
+	 *
+	 */
+
+}
+
+
+\
+
+
+
+
+
+public function __construct($profile...
