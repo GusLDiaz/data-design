5a451a36d174e56583c5137e7d372324d72e816a
Gus Liakos <Gliakos@cnm.edu>
2018-04-26 13:59:37 -0600

    update update statement *timestamp

--- sql/omlassignment.sql
+++ sql/omlassignment.sql
@@ -1,5 +1,6 @@
 INSERT INTO profile(profileId, profileEmail, profileHash, , profilePhone, profileUsername, profileTimestamp) VALUES
 	(UNHEX(REPLACE),"gusldiaz@yahoo.com", *prof, "5756379455","SwiggitySwag",*timestamp );
 
-UPDATE profile SET profilePhone = "5756379455",profileUsername = :SwiggitySwag,profileEmail = "gusldiaz@yahoo.com", profileHash = :profileHash, profileTimestamp = :profileTimestamp WHERE profileId = :profileId";
+UPDATE profile SET profilePhone = "5756379455",profileUsername = "SwiggitySwag",profileEmail = "gusldiaz@yahoo.com", profileHash = "", profileTimestamp = "1998-08-05 09:00:00",<--:profileTimestamp
+WHERE profileId = :profileId";
 
