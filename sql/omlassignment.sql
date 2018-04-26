1c38da5ce8d8fc3e2f96dd5e62e0a036a49e7214
Gus Liakos <Gliakos@cnm.edu>
2018-04-26 13:25:44 -0600

    initial Insert statement

---
+++ sql/omlassignment.sql
@@ -0,0 +1,4 @@
+INSERT INTO profile(profileId, profileEmail, profileHash, , profilePhone, profileUsername, profileTimestamp) VALUES
+	(UNHEX(REPLACE),"gusldiaz@yahoo.com", *prof, "5756379455","SwiggitySwag",*timestamp );
+
+
