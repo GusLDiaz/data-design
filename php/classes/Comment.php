59aaa013cc0a5042cd4cd0512858b37118d0f21e
Gus Liakos <Gliakos@cnm.edu>
2018-04-25 21:27:47 -0600

    create state vars and Constructor

--- php/classes/Comment.php
+++ php/classes/Comment.php
@@ -8,3 +8,39 @@
 /** php file for a comment (recursive-calling) class
  *
  */
+class Comment implements JSONSerializable {
+	use Ramsey\Uuid\Uuid;
+
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+		$fields["commentId"] = $this->commentId->toString();
+		unset($fields["profileHash"]);
+		return ($fields);
+	}
+//<h3>comment</h3>
+//<dl>
+//<dt>commentId</dt>
+//<dd>primary key</dd>
+//<dt>commentProfileId</dt>
+//<dd>foriegn key</dd>
+//<dt>commentCommentId</dt>
+//<dd>foreign key calling replys</dd>
+//<dt>commentContent</dt>
+//<dt>commmentTimestamp</dt>
+//<dd>register time</dd>
+//<dt>commentViaTag</dt>
+
+protected $commentId;
+protected $commentProfileId;
+protected $commentCommentId;
+protected $commentContent;
+protected $commentTimestamp;
+protected $commentViaTag;
+
+public function __construct($commentId, $commentProfileId, $commentCommentId, string $commmentContent, DateTime $commentTimestamp, ***int $commentViaTag) {
+		try {
+			$this->setCommentId($newCommentId);
+			$this->setCommentCommentId($newCommentCommentId);
+			$this->setCommentContent($newCommentContent);
+			$this->setCommentViaTag($newCommentViaTag);
+			$this->setCommentTimestamp($newCommentTimestamp);)
