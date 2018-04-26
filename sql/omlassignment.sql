8433dcdc8e82e0e9f6c2376e39916bd721c02c69
Gus Liakos <Gliakos@cnm.edu>
2018-04-26 15:39:39 -0600

    added last semicolon

--- sql/omlassignment.sql
+++ sql/omlassignment.sql
@@ -4,5 +4,5 @@ INSERT INTO profile(profileId, profileEmail, profileHash, , profilePhone, profil
 UPDATE profile SET profilePhone = "0000000000",profileUsername = "SwiggitySwag",profileEmail = "gusldiaz@yahoo.com", profileHash = "", profileTimestamp = "1998-08-05 10:00:00",<--:profileTimestamp
 WHERE profileId = "(UNHEX(REPLACE("8f767575-574c-4b07-9e84-39c05dcdbb2e","-",""));
 
-DELETE FROM profile WHERE profilePhone = "0000000000"
+DELETE FROM profile WHERE profilePhone = "0000000000";
 
