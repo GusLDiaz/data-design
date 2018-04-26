0c85e40503ceae15922c1d4d10dfed8fc6d25d9a
Gus Liakos <Gliakos@cnm.edu>
2018-04-26 17:01:29 -0600

    uncommented database name

--- sql/commentdb.sql
+++ sql/commentdb.sql
@@ -27,11 +27,10 @@ CREATE TABLE comment (
 	commentTimestamp DATETIME(6) NOT NULL,
 	commentViaTag TINYINT ,
 	INDEX(commentProfileId),
-	-- can you index something that's optional (notnull?)
 	INDEX(commentCommentId),
 	FOREIGN KEY(commentProfileId) REFERENCES profile(profileId),
 	FOREIGN KEY(commentCommentId) REFERENCES comment(commentId),
 	--analyse for recursion?
 	PRIMARY KEY(commentId)
-);
+	);
 
