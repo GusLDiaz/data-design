c27d285bbd9324a7d64713e59143382d21f6a60e
Gus Liakos <Gliakos@cnm.edu>
2018-04-26 13:46:13 -0600

    Add update statement *profhash

--- sql/omlassignment.sql
+++ sql/omlassignment.sql
@@ -1,4 +1,5 @@
 INSERT INTO profile(profileId, profileEmail, profileHash, , profilePhone, profileUsername, profileTimestamp) VALUES
 	(UNHEX(REPLACE),"gusldiaz@yahoo.com", *prof, "5756379455","SwiggitySwag",*timestamp );
 
+UPDATE profile SET profilePhone = "5756379455",profileUsername = :SwiggitySwag,profileEmail = "gusldiaz@yahoo.com", profileHash = :profileHash, profileTimestamp = :profileTimestamp WHERE profileId = :profileId";
 
