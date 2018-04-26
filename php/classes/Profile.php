e269926781bd6d612c54151ad717e783df251870
Gus Liakos <Gliakos@cnm.edu>
2018-04-26 15:43:13 -0600

    updated (commented) to-do list

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -5,16 +5,10 @@
  * Date: 4/19/2018
  * Time: 2:52 PM
  *
- * need: validate UUID (Ramsey)
- * state vars
- * constructor
  * doc blocks for it all
- *   oml assignment.sql
- * accessor &mutator methods for every state var
- * JsonSerializable
+ *   oml assignment.sql (select)
+ * accessor &mutator methods for every state va
  * get_by_
- *
- * update delete insert
  */
 
 namespace Edu\Cnm\DataDesign;
