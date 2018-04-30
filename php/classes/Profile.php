d7fdedcb94ae1bae8ab382a6757e81cc4d69d7c1
Gus Liakos <Gliakos@cnm.edu>
2018-04-30 01:29:35 -0600

    proper Doc Blocks, initial getFOObyBar()

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -24,13 +24,13 @@ class Profile implements \JsonSerializable {
 	 */
 	private $profileId;
 	/**
-	 * profile: Email (unique)
+	 * profile: Email
 	 * @var string $profileEmail
 	 */
 	protected $profileEmail;
 	/**
-	 * profile: hash-- what does this do?**
-	 * @var ****** $profileHash
+	 * profile: hash
+	 * @var string $profileHash
 	 */
 	protected $profileHash;
 	/**
@@ -39,6 +39,7 @@ class Profile implements \JsonSerializable {
 	 */
 	protected $profileUsername;
 	/**
+	 * profile: phone
 	 * @var string $profilePhone
 	 */
 	protected $profilePhone;
@@ -47,21 +48,12 @@ class Profile implements \JsonSerializable {
 	 */
 	protected $profileTimestamp;
 
-
-//public static function
-
-
-//
-//public function __construct($profileId,$profileTimeStamp,$profilePhone,$profileUsername,$profileEmail){
-//
-
-
 	/**
 	 * constructor
 	 *
 	 * @param string|Uuid
 	 * @param string hash is used for password encryption
-	 * @param string $newProfileEmail ? string containing  email address
+	 * @param string $newProfileEmail ? string containing email address
 	 * @param string $newProfileUsername ? string containing username
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
@@ -83,12 +75,19 @@ class Profile implements \JsonSerializable {
 		}
 	}
 
+	/**
+	 * accessor method for profile Id
+	 *
+	 * @return mixed Id
+	 */
 	public function getProfileId() {
 		return $this->profileId;
 	}
 
 	/**
+	 * mutator method for Id/primary key
 	 * @param mixed $profileId
+	 * @throws \Exception if uuid is invalid
 	 */
 	public function setProfileId($newProfileId): void {
 		try {
@@ -101,6 +100,8 @@ class Profile implements \JsonSerializable {
 	}
 
 	/**
+	 * accessor method for email
+	 *
 	 * @return string
 	 */
 	public function getProfileEmail(): string {
@@ -108,9 +109,10 @@ class Profile implements \JsonSerializable {
 	}
 
 	/**
-	 * sanatize
-	 * strlength
-	 * @param string $authorEmail
+	 * mutator method for email
+	 * @param string $profileEmail
+	 * @throws \InvalidArgumentException if email is empty
+	 * @throws \RangeException if email is too large for database
 	 */
 	public function setProfileEmail(string $newProfileEmail): void {
 		$newProfileEmail = trim($newProfileEmail);
@@ -118,7 +120,6 @@ class Profile implements \JsonSerializable {
 		if(empty($newProfileEmail) === true) {
 			throw(new \InvalidArgumentException("email is empty or insecure"));
 		}
-		// verify the email will fit in the database
 		if(strlen($newProfileEmail) > 128) {
 			throw(new \RangeException("profile email is too large"));
 		}
@@ -127,41 +128,88 @@ class Profile implements \JsonSerializable {
 	}
 
 	/**
-	 * what do you need for hash acc/mutator?
-	 * @return string
+	 * accessor method for hash
+	 *
+	 * @return string profile hash
 	 */
 	public function getProfileHash(): string {
 		return ($this->profileHash);
 	}
 
-	/**
-	 * @param string $profileHash
-	 */
-	public function setProfileHash(string $profileHash): void {
-		$this->profileHash = $profileHash;
+	//
+//	  mutator method for profile hash password
+//
+//	  @param string $newProfileHash
+//	  @throws \InvalidArgumentException if the hash is not secure
+//	  @throws \RangeException if the hash is not 128 characters
+//	  @throws \TypeError if profile hash is not a string
+	public function setProfileHash(string $newProfileHash): void {
+		//check hash is formatted
+		$newProfileHash = trim($newProfileHash);
+		if(empty($newProfileHash) === true) {
+			throw(new \InvalidArgumentException("profile password hash empty or insecure"));
+		}
+		//enforce the hash is really an Argon hash
+		$profileHashInfo = password_get_info($newProfileHash);
+		if($profileHashInfo["algoName"] !== "argon2i") {
+			throw(new \InvalidArgumentException("not a valid hash"));
+		}
+		//check hash is exactly 97 characters.
+		if(strlen($newProfileHash) !== 97) {
+			throw(new \RangeException("profile hash must be 97 characters"));
+		}
+		$this->profileHash = $newProfileHash;
 	}
 
 	/**
+	 * accessor method for username
+	 *
+	 * @return string $profileUsername
 	 */
 	public function getProfileUsername(): string {
 		return ($this->profileUsername);
 	}
 
 	/**
-	 * do this set sanatize string, etc.
+	 * mutator method for usernames
+	 *
 	 * @param string $profileUsername
+	 * @throws \InvalidArgumentException if username is null
+	 * @throws \RangeException if username is greater than 32 characters
 	 */
-	public function setProfileUsername(string $newProfileUsername): void {
+	public function setProfileUsername(?string $newProfileUsername): void {
+		//if $profileUsername is null return it right away
+		if($newProfileUsername === null) {
+			$this->profileUsername = null;
+			return;
+		}
+	$newProfileUsername = trim($newProfileUsername);
+	$newProfileUsername = filter_var($newProfileUsername, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	if(empty($newProfileUsername) === true) {
+		throw(new \InvalidArgumentException("username is empty "));
+	}
+	if(strlen($newProfileUsername) > 32) {
+		throw(new \RangeException("username is too long"));
+	}
 		$this->profileUsername = $newProfileUsername;
 	}
 
 	/**
+	 * accessor method for datetime
+	 *
+	 * @return \DateTime
 	 */
 	public function getProfileTimestamp(): \DateTime {
 		return ($this->profileTimestamp);
 	}
 
-	public function setProfileTimestamp(\DateTime $newProfileTimestamp): void {
+	/**
+	 * mutator method for datetime
+	 * validate date & time using magic method
+	 * @param null $newProfileTimestamp
+	 * @throws \Exception
+	 */
+	public function setProfileTimestamp($newProfileTimestamp = null): void {
 			// base case: if the date is null, use the current date and time
 			if($newProfileTimestamp === null) {
 				$this->profileTimestamp = new \DateTime();
@@ -178,7 +226,7 @@ class Profile implements \JsonSerializable {
 
 	/**
 	 * accessor method for phone
-	 *new profile phone or just phone
+	 * new profile phone or just phone
 	 * @return string value of phone or null
 	 **/
 	public function getProfilePhone(): ?string {
@@ -189,9 +237,9 @@ class Profile implements \JsonSerializable {
 	 * mutator method for phone
 	 *
 	 * @param string $newProfilePhone new value of phone
-	 * @throws \InvalidArgumentException if $newPhone is not a string or insecure
-	 * @throws \RangeException if $newPhone is > 32 characters
-	 * @throws \TypeError if $newPhone is not a string
+	 * @throws \InvalidArgumentException if $newProfilePhone is not a string or insecure
+	 * @throws \RangeException if $newProfilePhone is > 32 characters
+	 * @throws \TypeError if $newProfilePhone is not a string
 	 **/
 	public function setProfilePhone(?string $newProfilePhone): void {
 		//if $profilePhone is null return it right away
@@ -199,51 +247,23 @@ class Profile implements \JsonSerializable {
 			$this->profilePhone = null;
 			return;
 		}
-		// verify the phone is secure
 		$newProfilePhone = trim($newProfilePhone);
 		$newProfilePhone = filter_var($newProfilePhone, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($newProfilePhone) === true) {
 			throw(new \InvalidArgumentException("profile phone is empty or insecure"));
 		}
-		// verify the phone will fit in the database
 		if(strlen($newProfilePhone) > 32) {
 			throw(new \RangeException("profile phone is too large"));
 		}
-		// store the phone
 		$this->profilePhone = $newProfilePhone;
 	}
 
-	/**
-	 *
-	 * mutator method for profile hash password
-	 *
-	 * @param string $newProfileHash
-	 * @throws \InvalidArgumentException if the hash is not secure
-	 * @throws \RangeException if the hash is not 128 characters
-	 * @throws \TypeError if profile hash is not a string
-	 */
-//public function setProfileHash(string $newProfileHash): void {
-//	//enforce that the hash is properly formatted
-//	$newProfileHash = trim($newProfileHash);
-//	if(empty($newProfileHash) === true) {
-//		throw(new \InvalidArgumentException("profile password hash empty or insecure"));
-//	}
-//	//enforce the hash is really an Argon hash
-//	$profileHashInfo = password_get_info($newProfileHash);
-//	if($profileHashInfo["algoName"] !== "argon2i") {
-//		throw(new \InvalidArgumentException("profile hash is not a valid hash"));
-//	}
-//	//enforce that the hash is exactly 97 characters.
-//	if(strlen($newProfileHash) !== 97) {
-//		throw(new \RangeException("profile hash must be 97 characters"));
-//	}
-//	//store the hash
-//	$this->profileHash = $newProfileHash;
-//}
+
+
 
 
 	/**
-	 * inserts profile in SQL
+	 * inserts profile in SQL via PDO
 	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @throws \PDOException when mySQL related errors occur
@@ -276,12 +296,12 @@ VALUES :profileId, :profilePhone, :profileUsername, :profileEmail, :profileHash,
 	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
 	public function update(\PDO $pdo): void {
-		// create query template
 		$query = "UPDATE profile SET profilePhone = :profilePhone,profileUsername = :profileUsername,profileEmail = :profileEmail, profileHash = :profileHash, profileTimestamp = :profileTimestamp WHERE profileId = :profileId";
 		$statement = $pdo->prepare($query);
-		$parameters = ["profileId" => $this->profileId->getBytes(), "profileActivationToken" => $this->profileActivationToken, "profileAtHandle" => $this->profileAtHandle, "profileAvatarUrl" => $this->profileAvatarUrl, "profileEmail" => $this->profileEmail, "profileHash" => $this->profileHash, "profilePhone" => $this->profilePhone];
+		$parameters = ["profileId" => $this->profileId->getBytes(),"profileEmail" => $this->profileEmail, "profileHash" => $this->profileHash, "profilePhone" => $this->profilePhone, "profileUsername" => $this->profileUsername, "profileTimestamp"=> $this->profileTimestamp];
 		$statement->execute($parameters);
 	}
 
@@ -295,20 +315,21 @@ VALUES :profileId, :profilePhone, :profileUsername, :profileEmail, :profileHash,
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when a variable are not the correct data type
 	 **/
-//	public
-//	static function getProfileByProfileId(\PDO $pdo, string $profileId): ?Profile {
-//		// sanitize the profile id before searching
-//		try {
-//			$profileId = self::validateUuid($profileId);
-//		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-//			throw(new \PDOException($exception->getMessage(), 0, $exception));
-//		}
+	public
+	static function getProfileByProfileId(\PDO $pdo, string $profileId): ?Profile {
+
+		try {
+			$profileId = self::validateUuid($profileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+	}
+
 	/**
 	 * @return array resulting state variables to serialize
 	 *  state variables for JSON serialization
-	 * *unset hash?? add for token
+	 *
 	 **/
-
 	public function jsonSerialize(): array {
 		$fields = get_object_vars($this);
 		$fields["profileId"] = $this->profileId->toString();
